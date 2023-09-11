a48223af947c3fdace79294ce730baf0fa73d84f
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-09-11 18:34:31 -0300

    Criando as rotas dos posts

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { randomBytes } from 'crypto' 
+// import { bodyParser } from 'body-parser' // serve para certificar que sempre que um usuário enviar dados JSON no corpo de solicitação, eles realmente sejam passados e apareçam apropriadamente em maipulador de solicição  
+
+const app = express()
+const posts = {}
+app.use(/* bodyParser.json() */express.json())
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+
+  posts[id] = {
+    id, title
+  }
+
+  req.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+})
