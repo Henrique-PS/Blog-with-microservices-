f36929a26cf1ba60814fbd7ed282ee344f09bf37
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-09-18 00:03:22 -0300

    20. Building Post Submission

---
+++ client/src/components/PostCreate/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Form, Container } from 'react-bootstrap'
+import './styles.css'
+import axios from 'axios'
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('')
+
+  const onSubmit = async event => {
+    event.preventDefault()
+
+    await axios.post('http://localhost:5173/posts', { title })
+
+    setTitle('')
+  }
+
+  return (
+    <>
+      <Container>
+        <h1>Create Post</h1>
+
+        <Form onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button className="btn" type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    </>
+  )
+}
+
+export default PostCreate
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import PostCreate from './components/PostCreate/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <PostCreate/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
